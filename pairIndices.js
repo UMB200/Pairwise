7575a7c14f309c19b6bcf789ceb8bfe83951fa76
UMB200 <tomodachi200@gmail.com>
2017-10-23 13:14:41 -0400

    upload solution

--- pairIndices.js
+++ pairIndices.js
@@ -1,6 +1,16 @@
-
 function pairwise(arr, arg) {
-  return arg;
+  var result = 0;
+  var indexCounter = [];
+  for (var i =0 ; i < arr.length; i++){
+    for(var j= i+1; j< arr.length; j++){
+      if(arr[i] + arr[j] == arg & indexCounter.indexOf(i) == -1 && indexCounter.indexOf(j) == -1){
+      	indexCounter.push(i);
+      	indexCounter.push(j);
+      	result += i+ j;
+      }
+    }
+  }
+  return result;
 }
 
 pairwise([1,4,2,3,0,5], 7);
